3884be0278db4fb1c9eea53a39151a12fc33706c
Bogdan <56322180+Napoleon-Hub@users.noreply.github.com>
2020-03-21 22:09:42 +0300

    refactor: solution for HappyArray

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,36 @@
 package subtask1
 
 class HappyArray {
+    private var index: Int = 0
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var happyArray = sadArray
+        for (i in 1..3) {
+            happyArray = solution(happyArray)
+        }
+        return happyArray
+    }
+
+    fun solution(sadArray: IntArray): IntArray {
+        val happyArray = IntArray(sadArray.size)
+        for (elem in sadArray.indices) {
+            if (elem == sadArray.indices.last) {
+                happyArray[index] = sadArray[elem]
+                index = 0
+                break
+            }
+            if (elem == 0) {
+                happyArray[index] = sadArray[elem]
+                index++
+                continue
+            }
+            if (sadArray[elem] < sadArray[elem - 1] + sadArray[elem + 1]) {
+                happyArray[index] = sadArray[elem]
+                index++
+            }
+        }
+        val happyArray2: List<Int> = happyArray.filter { it != 0 }
+        return happyArray2.toIntArray()
     }
 }
