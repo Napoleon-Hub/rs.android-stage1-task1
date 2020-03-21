f1ba4612c4efdc28f74ab2f3f354e1a138cc289a
Bogdan <56322180+Napoleon-Hub@users.noreply.github.com>
2020-03-21 23:57:23 +0300

    refactor: solution for MiniMaxSum

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,6 +4,26 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var sumMin: Int = 10000
+        var sumMax: Int = 0
+        var sumCheck: Int = 0
+        var reserveArray = IntArray(input.size)
+        var reserveArray2 = input
+        for (i in 1..input.size) {
+            for (exem in reserveArray2.indices) {
+                if (exem != reserveArray2.lastIndex) {
+                    sumCheck += reserveArray2[exem]
+                    reserveArray[exem + 1] = reserveArray2[exem]
+                } else {
+                    sumMax = Math.max(sumMax, sumCheck)
+                    sumMin = Math.min(sumMin, sumCheck)
+                    sumCheck = 0
+                    reserveArray[0] = reserveArray2[exem]
+                }
+            }
+            reserveArray2 = reserveArray.clone()
+        }
+        val result = intArrayOf(sumMin, sumMax)
+        return result
     }
 }
